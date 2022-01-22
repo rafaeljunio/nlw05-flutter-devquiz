d1243ba54fac7ef8dc4081b2f422b3c398f5020f
Rafael Junio <rafaeljunio7@gmail.com>
2022-01-22 11:38:44 -0300

    Organizando o diretório

---
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -0,0 +1,44 @@
+import 'package:devquiz/challenge/widgets/awnser/awnser_widget.dart';
+import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/shared/models/question_model.dart';
+import 'package:devquiz/shared/models/anwser_model.dart';
+import 'package:flutter/material.dart';
+
+class QuizWidget extends StatefulWidget {
+  final QuestionModel question;
+  final VoidCallback onChange;
+  const QuizWidget({Key? key, required this.question, required this.onChange}) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+  AnwserModel awnser (int index) => widget.question.awnsers[index];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          SizedBox(height: 64),
+          Text(widget.question.title, style: AppTextStyles.heading,),
+          SizedBox(height: 24),
+          for (var i=0; i < widget.question.awnsers.length; i++)
+            AwnserWidget( //... operador de squid concatena a lista
+              awnser: awnser(i),
+              disabled: indexSelected != -1,
+              isSelected: indexSelected == i,
+              onTap: (){
+                indexSelected = i;
+                setState(() {});
+                Future.delayed(Duration(seconds: 2)).then((value) => widget.onChange());
+              },
+            ),  
+            
+        ],
+        ),
+      );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:devquiz/core/app_widget.dart';
+//import 'package:devquiz/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
